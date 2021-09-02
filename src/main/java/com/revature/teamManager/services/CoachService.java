186164f42e92133b771a3054a27fc7dcb9d6ce36
LunaHaines <nhaines95@gmail.com>
2021-09-02 15:03:51 -0600

    registration tests and service layer

--- src/main/java/com/revature/teamManager/services/CoachService.java
+++ src/main/java/com/revature/teamManager/services/CoachService.java
@@ -2,6 +2,7 @@ package com.revature.teamManager.services;
 
 import com.revature.teamManager.data.documents.Coach;
 import com.revature.teamManager.data.repos.CoachRepository;
+import com.revature.teamManager.util.exceptions.InvalidRequestException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +15,27 @@ public class CoachService {
     }
 
     public boolean isValid(Coach coach) {
-        return false;
+        if (coach.getCoachName().trim().equals("") || coach.getUsername().trim().equals("") ||
+                coach.getPassword().trim().equals("") || coach.getSport().trim().equals("") ||
+                coach.getTeamName().trim().equals("") || coach.getCoachName() == null ||
+                coach.getUsername() == null || coach.getPassword() == null || coach.getSport() == null ||
+                coach.getTeamName() == null) {
+            throw new InvalidRequestException("You must provide all necessary information");
+        }
+        if (coach.getPassword().length() < 8) {
+            throw new InvalidRequestException("Your password must be at least 8 characters long");
+        }
+        if (coachRepository.findByUsername(coach.getUsername()) != null) {
+            throw new InvalidRequestException("That username is already taken");
+        }
+        return true;
+    }
+
+    public Coach register(Coach coach) {
+        if (isValid(coach)) {
+            return coachRepository.save(coach);
+        }
+        return null;
     }
 
 }
