7ade32100b8b1e41030ca36b181371a28c0edcf4
LunaHaines <nhaines95@gmail.com>
2021-09-02 15:03:51 -0600

    registration tests and service layer

--- src/test/java/com/revature/teamManager/services/CoachServiceTestSuite.java
+++ src/test/java/com/revature/teamManager/services/CoachServiceTestSuite.java
@@ -1,7 +1,9 @@
 package com.revature.teamManager.services;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.revature.teamManager.data.documents.Coach;
 import com.revature.teamManager.data.repos.CoachRepository;
+import com.revature.teamManager.util.exceptions.InvalidRequestException;
 import org.junit.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
@@ -31,16 +33,18 @@ public class CoachServiceTestSuite {
         validCoach.setPassword("password");
         validCoach.setSport("Basketball");
         validCoach.setTeamName("Fighting TypeScripts");
+        when(mockCoachRepo.findByUsername(any())).thenReturn(null);
 
         // act
         boolean actualResult = sut.isValid(validCoach);
 
         // assert
         Assert.assertTrue("Expected coach to be considered valid",actualResult);
+        verify(mockCoachRepo,times(1)).findByUsername(any());
     }
 
-    @Test
-    public void isValid_returnsFalse_whenGivenEmptyName() {
+    @Test(expected = InvalidRequestException.class)
+    public void isValid_throwsInvalidRequestException_whenGivenEmptyName() {
         // arrange
         Coach invalidCoach = new Coach();
         invalidCoach.setCoachName("");
@@ -50,14 +54,15 @@ public class CoachServiceTestSuite {
         invalidCoach.setTeamName("Fighting TypeScripts");
 
         // act
-        boolean actualResult = sut.isValid(invalidCoach);
-
-        // assert
-        Assert.assertFalse("Expected coach to be considered invalid",actualResult);
+        try {
+            sut.isValid(invalidCoach);
+        } finally {
+            verify(mockCoachRepo, times(0)).findByUsername(any());
+        }
     }
 
-    @Test
-    public void isValid_returnsFalse_whenGivenEmptyUsername() {
+    @Test(expected = InvalidRequestException.class)
+    public void isValid_throwsInvalidRequestException_whenGivenEmptyUsername() {
         // arrange
         Coach invalidCoach = new Coach();
         invalidCoach.setCoachName("Bob");
@@ -67,14 +72,16 @@ public class CoachServiceTestSuite {
         invalidCoach.setTeamName("Fighting TypeScripts");
 
         // act
-        boolean actualResult = sut.isValid(invalidCoach);
-
-        // assert
-        Assert.assertFalse("Expected coach to be considered invalid",actualResult);
+        try {
+            sut.isValid(invalidCoach);
+        } finally {
+            // assert
+            verify(mockCoachRepo,times(0)).findByUsername(any());
+        }
     }
 
-    @Test
-    public void isValid_returnsFalse_whenGivenAlreadyExistingUsername() {
+    @Test(expected = InvalidRequestException.class)
+    public void isValid_throwsInvalidRequestException_whenGivenAlreadyExistingUsername() {
         // arrange
         Coach invalidCoach = new Coach();
         invalidCoach.setCoachName("Bob");
@@ -85,15 +92,16 @@ public class CoachServiceTestSuite {
         when(mockCoachRepo.findByUsername(any())).thenReturn(invalidCoach);
 
         // act
-        boolean actualResult = sut.isValid(invalidCoach);
-
-        // assert
-        verify(mockCoachRepo, times(1)).findByUsername(invalidCoach.getUsername());
-        Assert.assertFalse("Expected coach to be considered invalid",actualResult);
+        try {
+            sut.isValid(invalidCoach);
+        } finally {
+            // assert
+            verify(mockCoachRepo, times(1)).findByUsername(invalidCoach.getUsername());
+        }
     }
 
-    @Test
-    public void isValid_returnsFalse_whenGivenEmptyPassword() {
+    @Test(expected = InvalidRequestException.class)
+    public void isValid_throwsInvalidRequestException_whenGivenEmptyPassword() {
         // arrange
         Coach invalidCoach = new Coach();
         invalidCoach.setCoachName("Bob");
@@ -103,14 +111,16 @@ public class CoachServiceTestSuite {
         invalidCoach.setTeamName("Fighting TypeScripts");
 
         // act
-        boolean actualResult = sut.isValid(invalidCoach);
-
-        // assert
-        Assert.assertFalse("Expected coach to be considered invalid",actualResult);
+        try {
+            sut.isValid(invalidCoach);
+        } finally {
+            // assert
+            verify(mockCoachRepo,times(0)).findByUsername(any());
+        }
     }
 
-    @Test
-    public void isValid_returnsFalse_whenGivenPasswordLessThan8Characters() {
+    @Test(expected = InvalidRequestException.class)
+    public void isValid_throwsInvalidRequestException_whenGivenPasswordLessThan8Characters() {
         // arrange
         Coach invalidCoach = new Coach();
         invalidCoach.setCoachName("Bob");
@@ -120,14 +130,16 @@ public class CoachServiceTestSuite {
         invalidCoach.setTeamName("Fighting TypeScripts");
 
         // act
-        boolean actualResult = sut.isValid(invalidCoach);
-
-        // assert
-        Assert.assertFalse("Expected coach to be considered invalid",actualResult);
+        try {
+            sut.isValid(invalidCoach);
+        } finally {
+            // assert
+            verify(mockCoachRepo,times(0)).findByUsername(any());
+        }
     }
 
-    @Test
-    public void isValid_returnsFalse_whenGivenEmptySport() {
+    @Test(expected = InvalidRequestException.class)
+    public void isValid_throwsInvalidRequestException_whenGivenEmptySport() {
         // arrange
         Coach invalidCoach = new Coach();
         invalidCoach.setCoachName("Bob");
@@ -137,14 +149,16 @@ public class CoachServiceTestSuite {
         invalidCoach.setTeamName("Fighting TypeScripts");
 
         // act
-        boolean actualResult = sut.isValid(invalidCoach);
-
-        // assert
-        Assert.assertFalse("Expected coach to be considered invalid",actualResult);
+        try {
+            sut.isValid(invalidCoach);
+        } finally {
+            // assert
+            verify(mockCoachRepo,times(0)).findByUsername(any());
+        }
     }
 
-    @Test
-    public void isValid_returnsFalse_whenGivenEmptyTeamName() {
+    @Test(expected = InvalidRequestException.class)
+    public void isValid_throwsInvalidRequestException_whenGivenEmptyTeamName() {
         // arrange
         Coach invalidCoach = new Coach();
         invalidCoach.setCoachName("Bob");
@@ -154,9 +168,50 @@ public class CoachServiceTestSuite {
         invalidCoach.setTeamName("");
 
         // act
-        boolean actualResult = sut.isValid(invalidCoach);
+        try {
+            sut.isValid(invalidCoach);
+        } finally {
+            // assert
+            verify(mockCoachRepo,times(0)).findByUsername(any());
+        }
+    }
+
+    @Test
+    public void register_returnsSuccessfully_whenGivenValidCoach() {
+        // arrange
+        Coach validCoach = new Coach();
+        validCoach.setCoachName("Bob");
+        validCoach.setUsername("Bobby");
+        validCoach.setPassword("password");
+        validCoach.setSport("Basketball");
+        validCoach.setTeamName("Fighting TypeScripts");
+        when(mockCoachRepo.save(any())).thenReturn(validCoach);
+        when(mockCoachRepo.findByUsername(any())).thenReturn(null);
+
+        // act
+        Coach actualResult = sut.register(validCoach);
 
         // assert
-        Assert.assertFalse("Expected coach to be considered invalid",actualResult);
+        verify(mockCoachRepo, times(1)).save(any());
+        Assert.assertEquals(validCoach,actualResult);
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void register_returnsNull_whenCoachNotValid() {
+        // arrange
+        Coach invalidCoach = new Coach();
+        invalidCoach.setCoachName("");
+        invalidCoach.setUsername("Bobby");
+        invalidCoach.setPassword("password");
+        invalidCoach.setSport("Basketball");
+        invalidCoach.setTeamName("Fighting TypeScripts");
+        when(mockCoachRepo.save(any())).thenReturn(invalidCoach);
+        when(mockCoachRepo.findByUsername(any())).thenReturn(null);
+
+        // act
+        Coach actualResult = sut.register(invalidCoach);
+
+        // assert
+        Assert.assertEquals(null,actualResult);
     }
 }
