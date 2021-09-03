32cda00b89111b24534c397fa128ae2e38ebb2b0
LunaHaines <nhaines95@gmail.com>
2021-09-03 11:00:41 -0600

    coach registration works!

--- src/main/java/com/revature/teamManager/services/CoachService.java
+++ src/main/java/com/revature/teamManager/services/CoachService.java
@@ -3,6 +3,7 @@ package com.revature.teamManager.services;
 import com.revature.teamManager.data.documents.Coach;
 import com.revature.teamManager.data.repos.CoachRepository;
 import com.revature.teamManager.util.exceptions.InvalidRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +11,7 @@ public class CoachService {
 
     private CoachRepository coachRepository;
 
+    @Autowired
     public CoachService(CoachRepository coachRepository){
         this.coachRepository = coachRepository;
     }
