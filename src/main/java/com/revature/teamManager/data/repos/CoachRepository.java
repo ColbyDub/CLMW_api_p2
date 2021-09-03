7e808ee7c437ba2c282cc0b16b05b5aaa226b901
ColbyDub <cduball@outlook.com>
2021-09-03 10:40:58 -0500

    Refactored repos and fixed tests that used them

--- src/main/java/com/revature/teamManager/data/repos/CoachRepository.java
+++ src/main/java/com/revature/teamManager/data/repos/CoachRepository.java
@@ -1,39 +1,15 @@
 package com.revature.teamManager.data.repos;
 
 import com.revature.teamManager.data.documents.Coach;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class CoachRepository implements CrudRepository{
+public interface CoachRepository extends MongoRepository<Coach,String> {
 
-    @Override
-    public List findAll() {
-        return null;
-    }
+    Coach findCoachByUsernameAndPassword(String username, String password);
+    Coach findCoachByUsername(String username);
 
-    @Override
-    public Object findById(String id) {
-        return null;
-    }
-
-    public Coach findByUsername(String id) {
-        return null;
-    }
-
-    @Override
-    public Coach save(Object newResource) {
-        return null;
-    }
-
-    @Override
-    public boolean update(Object updatedResource) {
-        return false;
-    }
-
-    @Override
-    public boolean deleteById(int id) {
-        return false;
-    }
 }
--- src/main/java/com/revature/teamManager/data/repos/RecruiterRepository.java
+++ src/main/java/com/revature/teamManager/data/repos/RecruiterRepository.java
@@ -1,4 +1,15 @@
 package com.revature.teamManager.data.repos;
 
-public class RecruiterRepository implements CrudRepository{
+import com.revature.teamManager.data.documents.Recruiter;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RecruiterRepository extends MongoRepository<Recruiter,String> {
+
+    Recruiter findRecruiterByUsernameAndPassword(String username, String password);
+    Recruiter findRecruiterByUsername(String username);
+
 }
