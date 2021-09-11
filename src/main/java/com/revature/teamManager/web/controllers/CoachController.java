f38719163d7b759de9bc49aa2db455bbaa15dbac
LunaHaines <nhaines95@gmail.com>
2021-09-11 17:41:15 -0600

    wrote put mapping to assign player a position

--- src/main/java/com/revature/teamManager/web/controllers/CoachController.java
+++ src/main/java/com/revature/teamManager/web/controllers/CoachController.java
@@ -2,6 +2,7 @@ package com.revature.teamManager.web.controllers;
 
 import com.revature.teamManager.data.documents.Coach;
 import com.revature.teamManager.services.CoachService;
+import com.revature.teamManager.web.dtos.AssignPositionRequest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,5 +17,10 @@ public class CoachController {
     @PostMapping(produces = "application/json", consumes = "application/json")
     public Coach registerNewCoach(@RequestBody Coach coachCandidate) {
         return coachService.register(coachCandidate);
+    }
+
+    @PutMapping(value = "/positions", consumes = "application/json")
+    public void assignPlayerPosition(@RequestBody AssignPositionRequest req) {
+        coachService.assignPosition(req.getCoachUsername(), req.getPlayerUsername(), req.getPosition());
     }
 }
