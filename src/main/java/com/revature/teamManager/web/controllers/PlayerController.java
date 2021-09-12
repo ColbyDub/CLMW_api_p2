73c6bdcb951af1bd62a85b2dac9b16fa57f923de
LunaHaines <nhaines95@gmail.com>
2021-09-12 14:29:29 -0600

    accepting invites and fixed security aspect

--- src/main/java/com/revature/teamManager/web/controllers/PlayerController.java
+++ src/main/java/com/revature/teamManager/web/controllers/PlayerController.java
@@ -3,8 +3,11 @@ package com.revature.teamManager.web.controllers;
 import com.revature.teamManager.data.documents.Player;
 import com.revature.teamManager.services.PlayerService;
 import com.revature.teamManager.web.dtos.Offer;
+import com.revature.teamManager.web.dtos.PlayerDTO;
 import com.revature.teamManager.web.util.security.Secured;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/players")
@@ -20,13 +23,23 @@ public class PlayerController {
         return playerService.register(playerCandidate);
     }
 
-//    @Secured(allowedRoles = {"coach"})
+//    @Secured(allowedRoles = {"Coach"})
     @PutMapping(produces = "application/json", consumes = "application/json")
     public Player extendOffer(@RequestBody Offer newOffer){
         Player foundPlayer = playerService.updateOffers(newOffer);
         System.out.println(foundPlayer);
 
         return foundPlayer;
+    }
+
+    @Secured(allowedRoles = {"Player"})
+    @GetMapping(value = "{username}", produces = "application/json")
+    public PlayerDTO getInfo(@PathVariable String username) {
+        Player player = playerService.getPlayerInfo(username);
+
+        PlayerDTO response = new PlayerDTO(player);
+
+        return response;
     }
 
 }
