e15c6c9024538db21e60de84444e8059db061f94
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-14 12:26:10 -0600

    Players view team (#16)
    * find Coach/Team by Player Username
    
    * Assign workouts (#15)
    
    * Adding exercises to players now works
    
    * Updated secruity annotation and refactored
    
    * Updated the way getTeamPlayers works and added tests
    
    Co-authored-by: ColbyDub <55457941+ColbyDub@users.noreply.github.com>

--- src/main/java/com/revature/teamManager/data/repos/CoachRepository.java
+++ src/main/java/com/revature/teamManager/data/repos/CoachRepository.java
@@ -2,14 +2,15 @@ package com.revature.teamManager.data.repos;
 
 import com.revature.teamManager.data.documents.Coach;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CoachRepository extends MongoRepository<Coach,String> {
 
     Coach findCoachByUsernameAndPassword(String username, String password);
     Coach findCoachByUsername(String username);
+    @Query("{'players': {$elemMatch: {$elemMatch: {$in: [?0]}}}}")
+    Coach findCoachByPlayersContaining(String player);
 
 }
