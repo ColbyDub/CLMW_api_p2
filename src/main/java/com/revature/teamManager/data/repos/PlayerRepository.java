c3783de84b9aaf8b8cd49689616cd2089e83fd7e
ColbyDub <cduball@outlook.com>
2021-09-16 20:34:11 -0500

    Why didn't Git inform me of this merge CONFLICT??

--- src/main/java/com/revature/teamManager/data/repos/PlayerRepository.java
+++ src/main/java/com/revature/teamManager/data/repos/PlayerRepository.java
@@ -6,27 +6,15 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-import java.util.List;
-
-import java.util.List;
 
 @Repository
 public interface PlayerRepository extends MongoRepository<Player, String> {
-
-    //@Query("{username: ?}") Custom queries using this annotation
-
     Player findPlayerByUsernameAndPassword(String username, String password);
     Player findPlayerByUsername(String username);
-    @Query(value="{}",fields="{'username' : 1, 'name' : 1, 'teamName' : 1, 'invitations' : 1}")
-    List <Player> findAll();
 
     @Query(value="{}",fields="{'username' : 1, 'name' : 1, 'sports' : 1, 'teamName' : 1, 'skills' : 1, 'invitations' : 1}")
     List <Player> findAll();
     @Query("{'sports': {$elemMatch: {$in: [?0]}}}")
     List <Player> findPlayersBySport(String sport);
-
-//    Player updatePlayerOffers(String username){
-//        Player
-//    }
 
 }
